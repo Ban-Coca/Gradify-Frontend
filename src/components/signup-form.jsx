0fa1a0d1ab511ece906ac7fe0339fad4ef061515
Van Harvey Coca <vanharvey.coca@cit.edu>
2025-09-11 10:31:05 +0800

    corrected misspell

--- src/components/signup-form.jsx
+++ src/components/signup-form.jsx
@@ -204,7 +204,7 @@ export function SignupForm({ className, ...props }) {
         </motion.div>
         <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-background text-muted-foreground relative z-10 px-2">
-            Or singup with
+            Or register with
           </span>
         </div>
         <div className="grid gap-3 grid-cols-2">
